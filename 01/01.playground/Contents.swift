8d178ac88f4e797c18eeb3dd678f87e0ccb5c39b
by-bai <baihe.soh@gmail.com>
2021-04-11 01:37:13 +0800

    DAY 01: complete

---
+++ 01/01.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+// variables
+var str = "Hello, playground"
+str = "goodbye"
+
+
+// strings and integers
+var age = 38
+var population = 8_000_000
+
+
+// multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+// to only format code neatly & exclude line breaks, end each line with \
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+
+// doubles and booleans
+var pi = 3.141
+var awesome = true
+
+
+// string interpolation
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "The test results are here: \(str3)"
+
+
+// constants
+let taylor = "swift"
+
+
+// type interference
+let hello = "Hello, playground"
+// explicit type annotation
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
