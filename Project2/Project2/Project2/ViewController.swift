a63dbdf02f66948a8f510c1f160f5a78024caaf6
by-bai <baihe.soh@gmail.com>
2021-05-21 00:11:02 +0800

    DAY 19: in prog 2/3

---
+++ Project2/Project2/Project2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Bai on 20/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var Button1: UIButton!
+    @IBOutlet var Button2: UIButton!
+    @IBOutlet var Button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
