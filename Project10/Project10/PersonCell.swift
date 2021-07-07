2f29e85b15287dc431bc96c15cc0a9ea9ab7a0c9
by-bai <baihe.soh@gmail.com>
2021-07-07 23:03:38 +0800

    DAY 42: UICollectionView cells & data source (with bug)

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Bai on 7/7/21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var name: UILabel!
+    
+}
+
